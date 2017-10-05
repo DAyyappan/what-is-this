0d9222da68d19b5d9f618f87a8895741f4c05677
DAyyappan <dineshayyappan90@gmail.com>
2017-10-05 15:05:54 -0400

    Initialize class with negative pairings

--- testfile.py
+++ testfile.py
@@ -1,4 +1,20 @@
 #Test python file
 from bs4 import BeautifulSoup as bs
 import numpy as np
-# generate
+
+# generate set of students
+def initializeClass(classSize):
+    classIDs = np.zeros((classSize, 2))
+    print("classIDs shape = " + str(classIDs.shape))
+
+    return classIDs
+
+
+def populateNegatives(classList, numNegs = 2):
+    for student in range(len(classList)):
+        negatives = np.random.random_integers(1, len(classList), numNegs)
+        classList[student] = negatives
+
+    return classList
+
+print(populateNegatives(initializeClass(20)))
